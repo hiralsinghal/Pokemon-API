fa09fbff067bbfcdccae311432cd17521757f92f
hiralsinghal <singhal.hiral@gmail.com>
2026-02-13 00:32:59 +0530

    added more data

--- script.js
+++ script.js
@@ -18,11 +18,21 @@ function getPokemon(e) {
         const img = document.querySelector("#pokemonSprite");
         const nameH2 = document.querySelector("#displayName");
         const weightP = document.querySelector("#displayWeight");
+        const heightP = document.querySelector("#displayHeight");
+        const abilityP =document.querySelector("#displayAbility");
+        const baseExperienceP = document.querySelector("#displayBaseExperience")
+        const typeP = document.querySelector("#displayType");
+        const movesP = document.querySelector("#displayMoves");
 
         img.src = data.sprites.other["official-artwork"].front_default;
         img.alt = data.name;
         nameH2.textContent = capitalizeFirstLetter(data.name);
         weightP.textContent = `Weight: ${data.weight}`;
+        heightP.textContent = `Height: ${data.height}`;
+        abilityP.textContent = `Ability: ${capitalizeFirstLetter(data.abilities[0].ability.name)}`;
+        baseExperienceP.textContent = `Base Experience: ${data.base_experience}`;
+        typeP.textContent = `Type: ${capitalizeFirstLetter(data.types[0].type.name)}`;
+        movesP.textContent = `Moves: ${capitalizeFirstLetter(data.moves[0].move.name)}`;
     })
     .catch((err) => {
         console.log("Pokemon not found", err);
